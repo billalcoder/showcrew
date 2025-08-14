76cb79c23808917bd555a0f5b9e86ad341373c57
billalcoder <billalshekhani10@gmail.com>
2025-08-14 18:18:12 +0530

    builting somthing

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -51,14 +51,14 @@ export default function Cart() {
         ))}
 
         {/* Buttons */}
-        <div className="flex justify-between items-center mt-6">
+        {/* <div className="flex justify-between items-center mt-6">
           <button className="border border-black px-4 py-2 font-medium hover:bg-gray-100">
             ← CONTINUE SHOPPING
           </button>
           <button className="bg-gray-600 text-white px-4 py-2 font-medium rounded hover:bg-gray-700">
             UPDATE CART
           </button>
-        </div>
+        </div> */}
       </div>
 
       {/* Right: Summary */}
@@ -76,7 +76,7 @@ export default function Cart() {
           PROCEED TO CHECKOUT
         </button>
         <div className="mt-4 text-gray-600">
-          <span className="inline-flex items-center">
+          {/* <span className="inline-flex items-center">
             <svg
               className="w-4 h-4 mr-1 text-gray-500"
               fill="currentColor"
@@ -85,7 +85,7 @@ export default function Cart() {
               <path d="M9.049 2.927C9.469 1.622 11.532 1.622 11.951 2.927L13.236 6.875C13.39 7.329 13.781 7.668 14.26 7.745L18.455 8.39C19.834 8.607 20.358 10.409 19.251 11.31L16.217 13.74C15.843 14.031 15.678 14.512 15.798 14.964L16.835 18.92C17.216 20.29 15.686 21.365 14.499 20.58L11.27 18.473C10.865 18.22 10.312 18.22 9.907 18.473L6.678 20.58C5.491 21.365 3.961 20.29 4.342 18.92L5.379 14.964C5.499 14.512 5.334 14.031 4.96 13.74L1.926 11.31C0.819 10.409 1.343 8.607 2.722 8.39L6.917 7.745C7.396 7.668 7.787 7.329 7.941 6.875L9.226 2.927H9.049z" />
             </svg>
             Coupon
-          </span>
+          </span> */}
         </div>
       </div>
     </div>
