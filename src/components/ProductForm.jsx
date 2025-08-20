26666294271d1cb52f9cd02d829f64ffcb0f8d15
billalcoder <billalshekhani10@gmail.com>
2025-08-20 16:34:13 +0530

    completed my work

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -39,13 +39,13 @@ export default function ProductForm({ product, onSuccess }) {
       if (product?._id) {
         // UPDATE
         res = await axios.put(
-          `http://localhost:3000/products/${product._id}`,
+          `https://showcrew-backend.onrender.com/products/${product._id}`,
           formData,
           { withCredentials: true }
         );
       } else {
         // CREATE
-        res = await axios.post("http://localhost:3000/products", data, {
+        res = await axios.post("https://showcrew-backend.onrender.com/products", data, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         });
